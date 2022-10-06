323fda43846242a5547af3653f05851f6b75102f
DASH <shivansh.tyagi@vidyamantra.com>
2022-10-06 10:27:04 +0530

    Applied v-for

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,38 @@
 
 import { ref } from 'vue';
 
-const converion: { [key: string]: number } = { KWD: 0.31, BHD: 0.38, OMR: 0.39, GBP: 0.88, EUR: 1.01, USD: 1.00, BND: 1.43, NZD: 1.74, AWG: 1.80, INR: 81.40 };
+const converion: { [key: string]: [number, string] } = {
+    KWD: [0.31, 'Kuwaiti Dinar'],
+    BHD: [0.38, 'Bahraini Dinar'],
+    OMR: [0.39, 'Omani Rial'],
+    BP: [0.88, 'British Pound Sterling'],
+    EUR: [1.01, 'European Euro'],
+    USD: [1.00, 'US Dollar'],
+    BND: [1.43, 'Brunei Dollar'],
+    NZD: [1.74, 'New Zealand Dollar'],
+    AWG: [1.80, 'Aruban Florin'],
+    INR: [81.40, 'Indian Rupee']
+};
 const val1 = ref<number>(1);
 const val2 = ref<number>(81.4);
 const currency1 = ref<string>('USD');
 const currency2 = ref<string>('INR');
 
 const convertCurrency = () => {
-    val2.value = val1.value * converion[currency2.value] / converion[currency1.value];
+    val2.value = val1.value * converion[currency2.value][0] / converion[currency1.value][0];
 }
 
+// Exchange LHS and RHS select box values
 const exhange = () => {
-    let tmp: string | number = val1.value;
+    let tmpValue = val1.value;
     val1.value = val2.value;
-    val2.value = tmp;
-    tmp = currency1.value;
+    val2.value = tmpValue;
+    let tmpCurrency
+    tmpCurrency = currency1.value;
     currency1.value = currency2.value;
-    currency2.value = tmp;
+    currency2.value = tmpCurrency;
 }
+console.log(Object.keys(converion))
 
 
 </script>
@@ -28,20 +42,13 @@ const exhange = () => {
 
     <div class="flex gap-20 justify-center">
         <div class="flex flex-col w-40">
+            <!-- LHS DROPDOWN -->
             <select id="firstCurrency" v-model="currency1" class="w-40 h-6">
-                <option value="KWD">Kuwaiti Dinar</option>
-                <option value="BHD">Bahraini Dinar</option>
-                <option value="OMR">Omani Rial</option>
-                <option value="GBP">British Pound Sterling</option>
-                <option value="EUR">European Euro</option>
-                <option value="USD">US Dollar</option>
-                <option value="BND">Brunei Dollar</option>
-                <option value="NZD">New Zealand Dollar</option>
-                <option value="AWG">Aruban Florin</option>
-                <option value="INR">Indian Rupee</option>
+                <option v-for="(curr, index) in converion" :value="index" :key="index">{{curr[1]}}</option>
             </select>
             <input v-model="val1" type="number" class="border-2" />
         </div>
+        <!-- EXCHANGE BUTTON  -->
         <button @click="exhange">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="w-6 h-6">
@@ -51,21 +58,14 @@ const exhange = () => {
         </button>
 
         <div class="flex flex-col w-40">
+            <!-- RHS DROPDOWN  -->
             <select id="secondCurrency" v-model="currency2" class="w-40 h-6">
-                <option value="KWD">Kuwaiti Dinar</option>
-                <option value="BHD">Bahraini Dinar</option>
-                <option value="OMR">Omani Rial</option>
-                <option value="GBP">British Pound Sterling</option>
-                <option value="EUR">European Euro</option>
-                <option value="USD">US Dollar</option>
-                <option value="BND">Brunei Dollar</option>
-                <option value="NZD">New Zealand Dollar</option>
-                <option value="AWG">Aruban Florin</option>
-                <option value="INR">Indian Rupee</option>
+                <option v-for="(curr, index) in converion" :value="index" :key="index">{{curr[1]}}</option>
             </select>
             <input v-model="val2" type="number" class="border-2" />
         </div>
     </div>
+    <!-- CONVER CURRENCY BUTTON  -->
     <button @click="convertCurrency"
         class="w-20 border-2 h-8 mx-auto bg-slate-700 text-slate-100 rounded-full block my-10">Convert</button>
 
